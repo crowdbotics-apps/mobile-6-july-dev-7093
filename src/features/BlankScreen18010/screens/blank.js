a687c7d276066144aead3e5cce0b8834ce7f61ed
Crowdbotics <team@crowdbotics.com>
2020-07-06 11:04:12 +0000

    Update components

--- src/features/BlankScreen18010/screens/blank.js
+++ src/features/BlankScreen18010/screens/blank.js
@@ -381,6 +381,37 @@ export class _Blank extends React.Component {
       >
         Press me!
       </Button>
+      <Image
+        resizeMode="cover"
+        source={{
+          uri:
+            "https://d3nfddh2sbo9hk.cloudfront.net/media/project_component_resources/Chrysanthemum_ygPeF0o.jpg"
+        }}
+        style={{
+          width: 100,
+          height: 100,
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 0,
+          marginBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          fontFamily: "System"
+        }}
+      />
     </View>
   )
 }
